79d90e2ff69fe4cf485e9c5a917d95281f870044
Dima <dimasilolil@gmail.com>
2020-11-26 23:55:44 +0600

    First svg added

--- src/common-components/PlusButton/PlusButton.tsx
+++ src/common-components/PlusButton/PlusButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Plus from './../../../assets/icons/plus.svg';
+import {styles} from './styles';
 
 export const PlusButton: React.FC = () => {
-  return <Plus width={120} height={40} />;
+  return <Plus style={styles.plus} />;
 };
--- src/common-components/PlusButton/styles.ts
+++ src/common-components/PlusButton/styles.ts
@@ -2,13 +2,8 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   plus: {
-    padding: 15,
-    fontSize: 17,
-    lineHeight: 20,
-    color: '#514D47',
-    textAlign: 'center',
-    alignItems: 'center',
-    letterSpacing: -0.24,
-    fontFamily: 'SFUIText-Medium',
+    width: 16,
+    height: 16,
+    marginRight: 15,
   },
 });
