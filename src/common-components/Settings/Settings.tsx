41c457948ba40b98e2586c1250f7c82940d5b4e9
Dima <dimasilolil@gmail.com>
2020-11-29 23:47:39 +0600

    first buttoms added

---
+++ src/common-components/Settings/Settings.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import SettingsIcon from './../../../assets/icons/settings.svg';
+import {styles} from './styles';
+
+export const Settings: React.FC = () => {
+  return <SettingsIcon style={styles.icon} />;
+};
